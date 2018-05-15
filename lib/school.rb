0abc7d048d9c800f8a029164f967bb815327f2d0
JohnZBurnett <tuninator@gmail.com>
2018-05-15 09:04:58 -0400

    write methods for school class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+require 'pry'
+class School
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if !@roster[grade]
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+      end
+  end
+
+end
+
+flhs = School.new("Fairfield Ludlowe High School")
+flhs.add_student("John Burnett", 12)
+flhs.add_student("Mighty Mo", 12)
+flhs.add_student("Alph Green", 12)
+flhs.sort
